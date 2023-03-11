3d4ba166e73f09bb07e8f84672e15669929706e8
Agnes <koinageagness@gmail.com>
2023-03-11 17:40:25 +0300

    ft: create class based views

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from djago.urls import reverse_lazy
+from .models import TodoItem
 
 # Create your views here.
+
+
+class TodoListView(ListView):
+    model = TodoItem
+
+class TodoDetailView(DetailView):
+    model = TodoItem
+
+class TodoCreateView(CreateView):
+    model = TodoItem
+    fields = ["title", "description"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = TodoItem
+    fields = ["title", "description"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoDeleteView(DeleteView):
+    model = TodoItem
+    success_url = reverse_lazy("todo_list")
+
