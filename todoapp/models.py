83573ea6b68470bdbe96afa74c6b6e01198793d3
Agnes <koinageagness@gmail.com>
2023-03-11 21:40:23 +0300

    Add serializers, views_api.py and api endpoints

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class TodoItem(models.Model);
+class TodoItem(models.Model):
     title = models.CharField(max_length=255)
-    description = modles.TextField()
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated-at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self);
+    def __str__(self):
         return self.title
