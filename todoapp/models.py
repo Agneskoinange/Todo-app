943c58e1d9a7a814de1c41b90ac48f54264dc4a1
Agnes <koinageagness@gmail.com>
2023-03-11 17:00:00 +0300

    ft: Add todo model

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class TodoItem(models.Model);
+    title = models.CharField(max_length=255)
+    description = modles.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated-at = models.DateTimeField(auto_now=True)
+
+    def __str__(self);
+        return self.title
